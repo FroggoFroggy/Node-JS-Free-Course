3d1519f88ed671b40ea8d2a00d8f8fdc6edc4bfc
FroggoFroggy <anttorif@gmail.com>
2024-01-14 16:34:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 ServiceRouter.route("/").get((req, res) => {
-    res.send("Serviceka");
+    res.render("Serviceka");
 });
 
 ServiceRouter.route("/1").get((req, res) => {
