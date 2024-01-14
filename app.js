fd4f735a57d4a35ea020b9ce5dcb6241284e49b4
FroggoFroggy <anttorif@gmail.com>
2024-01-14 15:14:21 +0700

    create Multiple serviceSS

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const debug = require('debug')('app');
 const path = require('path')
 const app = express();
 const PORT = process.env.PORT;
+const ServiceRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/"))) //ถ้าตรงนี้ไม่ทำงาน จะเด้งไปแสดงผลที่ app.get("/",(req,res))
 
-app.set("views","./src/views");
-app.set("view engine","ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
+
+ServiceRouter.route("/").get((req, res) => {
+    res.send("Hi I'am ServiceSS of Steakholder");
+})
+
+ServiceRouter.route("/1").get((req, res) => {
+    res.send("Hi I'am ServiceSS 1 of Steakholder");
+})
+
+app.use("/ServiceSS", ServiceRouter)
 
 app.get("/", (req, res) => {
-    res.render('index',{username: 'MatoomInw', customers:["Dream","Poon","Em"]});
+    res.render('index', { username: 'MatoomInw', customers: ["Dream", "Poon", "Em"] });
 
 })
 app.listen(PORT, () => {
